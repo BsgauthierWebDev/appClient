f7625a1612479ed815a7195a9e55ab2e956ef363
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2021-01-02 10:06:00 -0500

    Add files via upload
    Initial file commit.

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import './App.css';
+import AppList from './appList/appList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apps:[],
+      search:'',
+      sort: '',
+      error: null
+    }
+  }
+
+  setSearch(search) {
+    this.setState({search});
+  }
+
+  setSort(sort) {
+    this.setState({sort});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const baseUrl = 'http://localhost.8000/apps';
+    const params = [];
+    if (this.state.search) {
+      params.push(`search = ${this.state.search}`);
+    }
+    if (this.state.sort) {
+      params.push(`sort = ${this.state.sort}`);
+    }
+    const query = params.join('&');
+    const url = `${baseUrl}?${query}`;
+
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          abbs: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: 'Could not get the apps at this time'
+        });
+      })
+  }
+
+  render() {
+    const apps = this.state.apps.map((app, i) => {
+      return <AppList {...app} key = {i}/>
+    })
+    return (
+      <main className = 'App'>
+        <h1>Google Play App List</h1>
+        <div className = 'search'>
+          <form onSubmit = {e => this.handleSubmit(e)}>
+            <label htmlFor = 'search'>Search: </label>
+            <input
+              type = 'text'
+              id = 'search'
+              name = 'search'
+              value = {this.state.search}
+              onChange = {e => this.setSearch(e.target.value)}/>
+
+            <label htmlFor = 'sort'>Sort: </label>
+            <select id = 'sort' name = 'sort' onChange = {e => this.setSort(e.target.value)}>
+              <option value = "">None</option>
+              <option value = 'rating'>Rating</option>
+              <option value = 'app'>App</option>
+            </select>
+            <button type = 'submit'>Search</button>
+          </form>
+          <div className = 'App_error'>{this.state.error}</div>
+        </div>
+        {apps}
+      </main>
+    );
+  }
+}
+
+export default App;
